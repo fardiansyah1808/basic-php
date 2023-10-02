a92a0a250e453f07d01444a2b4f05a69b73606f2
Fardiansyah <fardiansyah@ifca.co.id>
2023-10-02 11:07:06 +0700

    done

---
+++ RequireDanInclude.php
@@ -0,0 +1,9 @@
+<?php
+// include "Lib/MyFunction.php";
+include_once "Lib/MyFunction.php";
+// require "Lib/MyFunction.php";
+require_once "Lib/MyFunction.php";
+
+echo sayHello("Eko", "Kurniawan");
+
+ 
